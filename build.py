5a5127aee6c977e774374e00e7ac1f712067ad67
sunfkny <30853461+sunfkny@users.noreply.github.com>
2025-07-31 14:46:19 +0800

    Update build.py

--- build.py
+++ build.py
@@ -1,11 +1,26 @@
 import pathlib
 import subprocess
 import tomllib
+import typing
+
+
+class UvIndex(typing.TypedDict):
+    url: str
+    default: typing.NotRequired[str]
+    name: typing.NotRequired[str]
+
 
 with open("pyproject.toml", "rb") as f:
-    project_name = tomllib.load(f)["project"]["name"]
+    pyproject = tomllib.load(f)
 
+project_name = pyproject["project"]["name"]
 print(f"Building {project_name}...")
+
+uv_index: list[UvIndex] = pyproject["tool"]["uv"]["index"]
+uv_default_index = next(i for i in uv_index if "default" in i)["url"]
+
+GITHUB_MIRROR = "https://gh-proxy.com//https://github.com"
+UV_VERSION = "0.8.4"
 
 subprocess.run(
     [
@@ -25,17 +40,25 @@ subprocess.run(
         "--unzip-path",
         f"{project_name}-data",
         "--env",
-        "UV_PYTHON_INSTALL_MIRROR=https://gh-proxy.com/github.com/astral-sh/python-build-standalone/releases/download",
-        "--env",
-        "UV_DEFAULT_INDEX=https://mirrors.aliyun.com/pypi/simple",
-        "--env",
-        "UV_INSTALLER_GITHUB_BASE_URL=https://gh-proxy.com/github.com",
-        "--env",
         "UV_PYTHON_INSTALL_BIN=0",
-        "--uv-install-script-windows",
-        "https://gh-proxy.com/github.com/astral-sh/uv/releases/latest/download/uv-installer.ps1",
-        "--uv-install-script-unix",
-        "https://gh-proxy.com/github.com/astral-sh/uv/releases/latest/download/uv-installer.sh",
+        "--env",
+        f"UV_DEFAULT_INDEX={uv_default_index}",
+        *(
+            [
+                "--env",
+                f"UV_PYTHON_INSTALL_MIRROR={GITHUB_MIRROR}/astral-sh/python-build-standalone/releases/download",
+                "--env",
+                f"UV_INSTALLER_GITHUB_BASE_URL={GITHUB_MIRROR}",
+                "--uv-install-script-windows",
+                # f"{GITHUB_MIRROR}/astral-sh/uv/releases/latest/download/uv-installer.ps1",
+                f"{GITHUB_MIRROR}/astral-sh/uv/releases/download/{UV_VERSION}/uv-installer.ps1",
+                "--uv-install-script-unix",
+                # f"{GITHUB_MIRROR}/astral-sh/uv/releases/latest/download/uv-installer.sh",
+                f"{GITHUB_MIRROR}/astral-sh/uv/releases/download/{UV_VERSION}/uv-installer.sh",
+            ]
+            if GITHUB_MIRROR
+            else []
+        ),
     ],
     check=True,
 )
